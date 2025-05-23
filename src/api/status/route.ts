e8764a154edd24a16f5a0bfe7c85e5e5cbae6d59
wytxer <wy@bszhct.com>
2025-05-24 00:21:31 +0800

    up

---
+++ src/api/status/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export const runtime = 'edge'
+
+export const GET = async function (req: NextRequest) {
+  try {
+    return NextResponse.json({code: 0})
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json({code:500})
+  }
+}
