171d2088442587781ec047f52bdd55688eefa6e5
wytxer <wy@bszhct.com>
2025-05-24 14:44:32 +0800

    up

--- src/components/BigComponent.tsx
+++ src/components/BigComponent.tsx
@@ -3,8 +3,10 @@
 // todo 支持同时打包静态资源和 api
 // todo 目前的 api 写法打包会报错
 
+// todo 环境区分
+
 export default function BigComponent() {
-  const bigList = Array.from({ length: 132000 }, (_, i) => `Item ${i} - Lorem ipsum dolor sit amet, consectetur adipiscing elit.`)
+  const bigList = Array.from({ length: 332000 }, (_, i) => `Item ${i} - Lorem ipsum dolor sit amet, consectetur adipiscing elit.`)
 
   return (
     <div>
