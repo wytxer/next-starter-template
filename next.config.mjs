c23abe6ea6402f90e6d376cbc1bafa5ed48e446e
wytxer <wy@bszhct.com>
2025-05-25 17:58:17 +0800

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+
+initOpenNextCloudflareForDev();
+
 const nextConfig = {};
 
 export default nextConfig;
